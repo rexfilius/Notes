c532530be491686ce49bad932c20582c7d32ac62
Ifeakachukwu Osakwe <ifeakachukwuosakwe@gmail.com>
2021-11-13 16:08:21 +0100

    Write test for database DAO class

--- app/src/androidTest/java/com/github/rexfilius/notes/data/repository/source/local/NoteDaoTest.kt
+++ app/src/androidTest/java/com/github/rexfilius/notes/data/repository/source/local/NoteDaoTest.kt
@@ -1,69 +1,94 @@
-package com.github.rexfilius.notes.data
+package com.github.rexfilius.notes.data.repository.source.local
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.room.Room
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.github.rexfilius.notes.data.source.local.NotesDao
 import com.github.rexfilius.notes.data.source.local.NotesDatabase
-import com.github.rexfilius.notes.model.Note
 import com.github.rexfilius.notes.getOrAwaitValue
+import com.github.rexfilius.notes.model.Note
 import com.google.common.truth.Truth.assertThat
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
+import javax.inject.Inject
+import javax.inject.Named
 
 // @SmallTest annotation is optional
 @ExperimentalCoroutinesApi
-@RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
 @SmallTest
 class NoteDaoTest {
 
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    private lateinit var notesDatabase: NotesDatabase
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    @Named("notes_test_db")
+    lateinit var notesDatabase: NotesDatabase
+
     private lateinit var notesDao: NotesDao
 
     @Before
-    fun createDatabase() {
-        notesDatabase =
-            Room.inMemoryDatabaseBuilder(
-                ApplicationProvider.getApplicationContext(),
-                NotesDatabase::class.java
-            ).allowMainThreadQueries()
-                .build()
+    fun setup() {
+        hiltRule.inject()
         notesDao = notesDatabase.notesDao
     }
 
     @After
-    fun closeDatabase() {
+    fun tearDown() {
         notesDatabase.close()
     }
 
     @Test
-    fun insertNote() = runBlockingTest {
+    fun insert() = runBlockingTest {
         val note = Note("Note 1", "Desc 1", 1)
         notesDao.insert(note)
 
         val allNotes = notesDao.getAllNotes().getOrAwaitValue()
-
         assertThat(allNotes).contains(note)
     }
 
     @Test
-    fun deleteNote() = runBlockingTest {
+    fun update() = runBlockingTest {
+        val note = Note("Note 1", "Desc 1", 1)
+        notesDao.insert(note)
+
+        note.description = "Desc 2"
+        notesDao.update(note)  // if this line is removed, the test still passes
+
+        assertThat(note.description).isEqualTo("Desc 2")
+    }
+
+    @Test
+    fun delete() = runBlockingTest {
         val note = Note("Note 1", "Desc 1", 1)
         notesDao.insert(note)
         notesDao.delete(note)
 
         val allNotes = notesDao.getAllNotes().getOrAwaitValue()
         assertThat(allNotes).doesNotContain(note)
+    }
+
+    @Test
+    fun deleteAllNotes() = runBlockingTest {
+        val note1 = Note("Note 1", "Desc 1", 1)
+        val note2 = Note("Note 2", "Desc 2", 2)
+
+        notesDao.insert(note1)
+        notesDao.insert(note2)
+
+        notesDao.deleteAllNotes()
+
+        val allNotes = notesDao.getAllNotes().getOrAwaitValue()
+        assertThat(allNotes).isEmpty()
     }
 
     @Test
@@ -77,7 +102,15 @@ class NoteDaoTest {
         notesDao.insert(note3)
 
         val allNotes = notesDao.getAllNotes().getOrAwaitValue()
-
         assertThat(allNotes).containsExactly(note1, note2, note3)
+    }
+
+    @Test
+    fun getNote() = runBlockingTest {
+        val note1 = Note("Note 1", "Desc 1", 1)
+        notesDao.insert(note1)
+
+        val note = notesDao.getNote(1).getOrAwaitValue()
+        assertThat(note1.id).isEqualTo(note.id)
     }
 }
